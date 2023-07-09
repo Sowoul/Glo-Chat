196be28944a6cfb1374e962833f72e3f0dd99d3c
Ojaswa Sharma <93905595+Sowoul@users.noreply.github.com>
2023-07-09 16:59:36 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from flask_socketio import SocketIO, emit
 # Create the Flask app
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret_key'
-socketio = SocketIO(app)
+socketio = SocketIO(app, async_mode='threading')
 
 # Store connected clients
 clients = []
@@ -43,4 +43,5 @@ def message(data):
 
 if __name__ == '__main__':
     # Run the app with SocketIO
-    socketio.run(app, host='0.0.0.0', port=5000, debug=True, log_output=True, workers=4, timeout=60)
+    socketio.run(app, host='0.0.0.0', port=5000, debug=True)
+
